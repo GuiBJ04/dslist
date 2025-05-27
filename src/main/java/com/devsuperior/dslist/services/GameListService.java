cf1887d24b4bb9af795bfb558d7422c574ae9c9c
GuiBJ04 <guilhermebizonjeronymo@gmail.com>
2025-05-27 10:59:48 -0300

    Fixing imports

--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -1,10 +1,7 @@
 package com.devsuperior.dslist.services;
 
 
-import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameListDTO;
-import com.devsuperior.dslist.dto.GameMinDTO;
-import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameListRepository;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
